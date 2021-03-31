16574808663a9e1dca546f51915f476044df9549
Alan Claughan <git@claughan.com>
2021-03-31 21:26:37 +0200

    added movement controls

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ scr.title(titlestring = "my snake game")
 scr.tracer(0)
 
 snake = Snake()
+scr.listen()
+scr.onkey(snake.up, "Up")
+scr.onkey(snake.down, "Down")
+scr.onkey(snake.left, "Left")
+scr.onkey(snake.right, "Right")
 
 game_started = True
 
